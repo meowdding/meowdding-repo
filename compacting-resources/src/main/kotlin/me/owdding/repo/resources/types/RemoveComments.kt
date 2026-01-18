f9a5957c350b9b8c4b7db9b1373ede444465dc88
Mona <git@mona.cat>
2026-01-18 23:14:34 +0100

    fix: dont override remove comments

--- compacting-resources/src/main/kotlin/me/owdding/repo/resources/types/RemoveComments.kt
+++ compacting-resources/src/main/kotlin/me/owdding/repo/resources/types/RemoveComments.kt
@@ -5,8 +5,14 @@ import com.google.gson.JsonElement
 import me.owdding.repo.resources.ResourceType
 
 class RemoveComments(val file: String, output: String) : ResourceType<JsonElement>(::JsonArray, output) {
+    private var hasFound = false
+
     override fun add(fileName: String, element: JsonElement) {
-        if (fileName == file.substringAfterLast("/")) value = element
+        if (hasFound) return
+        if (fileName == file.substringAfterLast("/")) {
+            value = element
+            hasFound = true
+        }
     }
 
     override fun getPath() = arrayOf("$file.jsonc")
