a9a6070eb35ad97dd303ee6cbdd4c65f84735a6b
Mona <git@mona.cat>
2026-01-18 23:00:06 +0100

    fix: not setting jvm versions correctly

--- compacting-resources/build.gradle.kts
+++ compacting-resources/build.gradle.kts
@@ -52,3 +52,12 @@ publishing {
         }
     }
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    jvmToolchain(21)
+}
