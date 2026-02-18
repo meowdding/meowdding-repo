f2d24e89ab57c8622ab5ab96a18e506e7b87f5e4
Mona <git@mona.cat>
2026-02-18 21:05:17 +0100

    feat: add neu data extractor

---
+++ .github/scripts/update_neu.mjs
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+async function update_skulls() {
+    let skulls = await fetch("https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/refs/heads/master/constants/animatedskulls.json")
+        .then((x) => x.json())
+
+    let skins = skulls["skins"]
+    for (let key in skins) {
+        let skin = skins[key]
+        if (skin["textures"].length == 1) {
+            skin.ticks = undefined
+        }
+    }
+    skins["FERMENTO_ULTIMATE"] = undefined
+
+    fs.writeFileSync(`repo/skyocean/skulls.json`, JSON.stringify(skins, null, 2));
+}
+
+async function update_dyes() {
+
+    let dyes = await fetch("https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/refs/heads/master/constants/dyes.json")
+        .then((x) => x.json())
+
+    fs.writeFileSync(`repo/skyocean/dyes.json`, JSON.stringify(dyes, null, 2));
+}
+
+async function main() {
+    await update_dyes()
+    await update_skulls()
+}
+
+await main()
